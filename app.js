c1187ff258ed5bf08bd6c5debfbd0e73c1f4bffd
Amandeep Bal <amandeepbal@gmail.com>
2016-07-14 21:31:42 -0400

    port changes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
+app.listen((process.env.PORT || 3000), function () {
   console.log('Example app listening on port 3000!');
 });
