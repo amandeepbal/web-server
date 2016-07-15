df2d0f29d3e9a3845001cb702f8f465b1efa7e98
Amandeep Bal <amandeepbal@gmail.com>
2016-07-14 21:20:48 -0400

    sss

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(port, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 });
